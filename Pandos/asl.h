dce4b238b80d3364ce98a16d15e2ac63600494dd
anubis09 <60443037+anubis09@users.noreply.github.com>
2021-02-15 23:08:33 +0100

    Update asl.h

--- Pandos/asl.h
+++ Pandos/asl.h
@@ -4,4 +4,31 @@
 #include <pandos_const.h>
 #include <pandos_types.h>
 
+/*semaphore descriptor type*/
+typedef struct semd_t {
+    struct semd_t *s_next; /*next element on the ASL*/
+    int  *s_semAdd; /*pointer to the semaphore*/
+    pcb_PTR  s_procQ; /*tail pointer to a process queue */
+} semd_t;
+
+void initASL();
+
+/*inizializza il semaforo passato come primo parametro
+con i valori passati come semAdd e next */
+HIDDEN void initSemaphore(semd_t *,int *,semd_t *);
+
+/*cerca nella ASL per un descrittore del semaforo, se lo trova lo ritorna
+senno ritorna il semaforo nella ASL con descrittore di valore inferiore
+più vicino a quello dato in input*/
+HIDDEN semd_t *searchSem(int *);
+
+/*prende un semaforo da lista libera 
+lo inizializza con semAdd e lo restituisce
+se lista libera è vuota restituisce NULL*/
+HIDDEN semd_t *allocSem(int *semAdd);
+int insertBlocked(int *, pcb_PTR );
+pcb_PTR removeBlocked(int *);
+pcb_PTR outBlocked(pcb_PTR );
+pcb_PTR headBlocked(int *);
+
 #endif
