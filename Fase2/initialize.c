2013cfd3d367f7967d91346d6feb87526f15a110
Kr0m3s <79373883+Kr0m3s@users.noreply.github.com>
2021-03-18 18:01:48 +0100

    Update initialize.c

--- Fase2/initialize.c
+++ Fase2/initialize.c
@@ -1,5 +1,15 @@
+/*Pass Up Vector*/
+typedef struct passupvector { 
+	unsigned int tlb_refill_handler;
+	unsigned int tlb_refill_stackPtr;
+	unsigned int exception_handler;
+	unsigned int exception_stackPtr; } 
+passupvector_t;
 
 
-int main(){
-
+HIDDEN void iniPassUPvector(passupvector_t *puvec){
+    puvec->tlbrefllhandler = (memaddr) uTLBRefillHandler;
+    puvec->tlb_refill_stackPtr = KERNELSTACK;
+    puvec->exceptionhandler = (memaddr) fooBar;
+    puvec->exception_stackPtr = KERNELSTACK;
 }
